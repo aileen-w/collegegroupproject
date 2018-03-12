baff4965a7e263fa5db25d3dc2bbca34042e9cc8
kamil-pe <36553862+kamil-pe@users.noreply.github.com>
2018-03-12 13:02:27 +0000

    Update const.php
    remove username and password from file

--- web_management/config/const.php
+++ web_management/config/const.php
@@ -2,8 +2,8 @@
 
 define("HOST", "mysql");
 define("DB", "cit");
-define("USER", "jmc");
-define("PASS", "diehard2018");
+define("USER", "");
+define("PASS", "");
 define("CHARSET", "utf8mb4");
 
 define("SALT", "frY5YRaf]c87WAMTQp7:ZCB,(JD%uRH>WLK8d8P8+zg.G)&V/A#vR:qVc<a3jTSF(7%[GgvVD3d#\axqSRM_x^\[4;{^c&B/SMj>]9nTB\"G<KVH#8e&L2BVme8T*a'q");
