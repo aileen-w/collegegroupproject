e881a5378cd53ef5d1ec2ba92b2d8fa801406012
kamil-pe <kamil.piecuch@mycit.ie>
2018-04-22 16:27:34 +0100

    implement calendar calls to api

--- web_management/api.php
+++ web_management/api.php
@@ -24,7 +24,6 @@ include_once ("config/functions.php");
 
     if($_SERVER['REQUEST_METHOD'] === 'POST')
     {
-
         // variables
         $post = json_decode(file_get_contents('php://input'), true);
         $svc = !empty($post['svc']) ? $post['svc'] : null;
@@ -48,6 +47,167 @@ include_once ("config/functions.php");
                     $sql = "INSERT INTO geolocation(device, date, latitude, longitude) VALUES(:device, NOW(), :latitude, :longitude);";
                     $params = array("device" => $dev, "latitude" => $latitude, "longitude" => $longitude);
                     $result = insertDB($pdo, $sql, $params);
+                    break;
+                // insert calendar information to DB
+                case "calendar":
+                    $cal = json_decode($msg, true);
+                    $user = $cal['user'];
+                    $title = $cal['title'];
+                    $desc = $cal['desc'];
+                    $date = $cal['date'];
+                    $t_from = $cal['t_from'];
+                    $t_to = $cal['t_to'];
+                    $active = !empty($cal['active']) ? $cal['active'] : null;
+                    $id = !empty($cal['id']) ? $cal['id'] : null;
+                    $action = $cal['action'];
+                    if(!empty($action)){
+                        if($action === 'new'){
+
+                            if( empty($title) ||
+                                empty($desc) ||
+                                empty($date) ||
+                                empty($t_from) ||
+                                empty($t_from) ||
+                                empty($user)
+                            ){
+                                errorLog($pdo, "API:Calendar, empty value passed (line: " . __LINE__ . ")");
+                                $result = array("status" => "Error", "msg" => "Calendar, found empties.");
+                            };
+
+                            $sql = "INSERT  INTO calendar(user_device, title, cal_desc, cal_date, time_from, time_to) 
+                                            VALUES(:user, :title, :desc, :date, :t_from, :t_to);";
+                            $params = array(
+                                "user" => $user,
+                                "title" => $title,
+                                "desc" => $desc,
+                                "date" => $date,
+                                "t_from" => $t_from,
+                                "t_to" => $t_to
+                            );
+                            $result = insertDB($pdo, $sql, $params);
+                        }
+                        if($action === 'delete'){
+
+                            if(empty($id)){
+                                errorLog($pdo, "API:Calendar, empty value passed (line: " . __LINE__ . ")");
+                                $result = array("status" => "Error", "msg" => "Calendar, found empties.");
+                            };
+
+                            $sql = "UPDATE calendar SET active = 'N' WHERE id = :id;";
+                            $params = array(
+                                "id" => $id
+                            );
+                            $result = insertDB($pdo, $sql, $params);
+                        }
+                        if($action === 'update'){
+
+                            if( empty($title) ||
+                                empty($desc) ||
+                                empty($date) ||
+                                empty($t_from) ||
+                                empty($t_from) ||
+                                empty($id)
+                            ){
+                                errorLog($pdo, "API:Calendar, empty value passed (line: " . __LINE__ . ")");
+                                $result = array("status" => "Error", "msg" => "Calendar, found empties.");
+                            };
+
+                            $sql = "UPDATE calendar 
+                                    SET title = :title,
+                                        cal_desc = :desc,
+                                        cal_date = :date,
+                                        time_from = :t_from,
+                                        time_to = :t_to
+                                    WHERE id = :id;";
+                            $params = array(
+                                "title" => $title,
+                                "desc" => $desc,
+                                "date" => $date,
+                                "t_from" => $t_from,
+                                "t_to" => $t_to,
+                                "id" => $id
+                            );
+                            $result = insertDB($pdo, $sql, $params);
+                        }
+                        if($action === 'view'){
+
+                            if( empty($user) ||
+                                empty($date)
+                            ){errorLog(
+                                $pdo, "API:Calendar, empty value passed (line: " . __LINE__ . ")");
+                                $result = array("status" => "Error", "msg" => "Calendar, found empties.");
+                            };
+
+                            $sql = "SELECT * FROM calendar
+                                    WHERE user_device = :user 
+                                    AND cal_date = :date 
+                                    AND active = 'Y'
+                                    ORDER BY cal_date, time_from;";
+                            $params = array(
+                                "user" => $user,
+                                "date" => $date
+                            );
+                            $result = selectDB($pdo, $sql, $params);
+                        }
+                        if($action === 'viewOne'){
+
+                            if(empty($id)){
+                                errorLog($pdo, "API:Calendar, empty value passed (line: " . __LINE__ . ")");
+                                $result = array("status" => "Error", "msg" => "Calendar, found empties.");
+                            };
+
+                            $sql = "SELECT * FROM calendar
+                                    WHERE id = :id;";
+                            $params = array(
+                                "id" => $id
+                            );
+                            $result = selectDB($pdo, $sql, $params);
+                        }
+                        if($action === 'next'){
+
+                            if( empty($user) ||
+                                empty($id)
+                            ){errorLog(
+                                $pdo, "API:Calendar, empty value passed (line: " . __LINE__ . ")");
+                                $result = array("status" => "Error", "msg" => "Calendar, found empties.");
+                            };
+
+                            $sql = "SELECT * FROM calendar
+                                    WHERE user_device = :user 
+                                    AND id = (select min(id) from calendar where user_device = :user and id > :id) 
+                                    AND active = 'Y'
+                                    LIMIT 1;";
+                            $params = array(
+                                "user" => $user,
+                                "id" => $id
+                            );
+                            $result = selectDB($pdo, $sql, $params);
+                        }
+                        if($action === 'prev'){
+
+                            if( empty($user) ||
+                                empty($id)
+                            ){errorLog(
+                                $pdo, "API:Calendar, empty value passed (line: " . __LINE__ . ")");
+                                $result = array("status" => "Error", "msg" => "Calendar, found empties.");
+                            };
+
+                            $sql = "SELECT * FROM calendar
+                                    WHERE user_device = :user 
+                                    AND id = (select max(id) from calendar where user_device = :user and id < :id) 
+                                    AND active = 'Y'
+                                    LIMIT 1;";
+                            $params = array(
+                                "user" => $user,
+                                "id" => $id
+                            );
+                            $result = selectDB($pdo, $sql, $params);
+                        }
+
+                        if($result){
+                            $result = array("status" => "Ok", "msg" => $result);
+                        }
+                    }
                     break;
                 // insert error received from android app into DB
                 case "error":
