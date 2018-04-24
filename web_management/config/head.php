2cba447e0840834921f42e2710d085d923dd0d73
kamil-pe <kamil.piecuch@mycit.ie>
2018-04-24 23:47:12 +0100

    bugfix

--- web_management/config/head.php
+++ web_management/config/head.php
@@ -12,20 +12,20 @@
     <title>VCE - CIT Group Project</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="../web_management/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="../web_management/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="../web_management/dist/css/sb-admin-2.css" rel="stylesheet">
-    <link href="../web_management/css/cit-group-project.css" rel="stylesheet">
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="../css/cit-group-project.css" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
-    <link href="../web_management/vendor/morrisjs/morris.css" rel="stylesheet">
+    <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="../web_management/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
